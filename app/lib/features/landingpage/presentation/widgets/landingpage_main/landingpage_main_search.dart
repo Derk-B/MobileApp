fdfa9d5fac077ee5758ac5ded5c0575d4fe10fd8
Derk Blom <74244634+Derk-B@users.noreply.github.com>
2024-01-02 21:56:47 +0100

    Fixed sizing problem in searchbar

--- app/lib/features/landingpage/presentation/widgets/landingpage_main/landingpage_main_search.dart
+++ app/lib/features/landingpage/presentation/widgets/landingpage_main/landingpage_main_search.dart
@@ -19,49 +19,53 @@ class _LandingPageMainSearchState extends State<LandingPageMainSearch> {
         padding: const EdgeInsets.all(4.0),
         decoration: BoxDecoration(
           borderRadius: BorderRadius.circular(50.0),
-          border: Border.all(color: Colors.black, width: 0.5),
+          border: Border.all(color: Colors.black, width: 1),
         ),
         child: Form(
-          child: Stack(
-            children: [
-              TextField(
-                controller: _controller,
-                onChanged: (value) =>
-                    setState(() => _showClearIcon = value.isNotEmpty),
-                decoration: const InputDecoration(
-                  contentPadding:
-                      EdgeInsets.symmetric(vertical: 12.0, horizontal: 8.0),
-                  hintText: "Adres, b.v. Amstelplein 10",
-                  border: OutlineInputBorder(borderSide: BorderSide.none),
-                ),
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.end,
-                children: [
-                  _showClearIcon
-                      ? IconButton(
-                          onPressed: () {
-                            _controller.clear();
-                            setState(() {
-                              _showClearIcon = false;
-                            });
-                          },
-                          icon: const Icon(Icons.clear),
-                        )
-                      : Container(),
-                  TextButton(
-                    child: Text(
-                      "    Zoek    ",
-                      style: TextStyle(
-                        fontSize: 20,
-                        color: Theme.of(context).colorScheme.onPrimary,
+          child: SizedBox(
+            height: 50,
+            child: Row(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                Expanded(
+                  child: Align(
+                    alignment: Alignment.center,
+                    child: TextField(
+                      controller: _controller,
+                      onChanged: (value) =>
+                          setState(() => _showClearIcon = value.isNotEmpty),
+                      decoration: const InputDecoration(
+                        contentPadding: EdgeInsets.symmetric(
+                            vertical: 12.0, horizontal: 8.0),
+                        hintText: "Adres, b.v. Amstelplein 10",
+                        border: OutlineInputBorder(borderSide: BorderSide.none),
                       ),
                     ),
-                    onPressed: () {},
                   ),
-                ],
-              ),
-            ],
+                ),
+                Opacity(
+                    opacity: _showClearIcon ? 1 : 0,
+                    child: IconButton(
+                      onPressed: () {
+                        _controller.clear();
+                        setState(() {
+                          _showClearIcon = false;
+                        });
+                      },
+                      icon: const Icon(Icons.clear),
+                    )),
+                TextButton(
+                  child: Text(
+                    "    Zoek    ",
+                    style: TextStyle(
+                      fontSize: 20,
+                      color: Theme.of(context).colorScheme.onPrimary,
+                    ),
+                  ),
+                  onPressed: () {},
+                ),
+              ],
+            ),
           ),
         ),
       ),
